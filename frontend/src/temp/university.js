2b462b5b1a03c0c13e8f9fadc15094d340b385f9
Naim <skylike2904@gmail.com>
2023-03-14 13:40:49 +0600

    fixed some texts/headings

--- frontend/src/temp/university.js
+++ frontend/src/temp/university.js
@@ -1,10 +1,116 @@
 export const courses = [
   {
     name: "Programming in C",
-    code: "CSC 131",
+    code: "CSC 183",
     classes: [
       {
+        name: "Section A",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {
+          "Students facing problems to understand accent": "50%",
+          "Students prefer to write down on the board": "37%",
+          "Students are requesting to solve the practice problems more often":
+            "39%",
+          "Students want to get depth knowledge in ML": "20%",
+          "Students couldn't find the course materials in University Library":
+            "10%",
+          "Students suggest faculty vary their teaching methods": "5%",
+        },
+      },
+
+      {
         name: "Section B",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section C",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section D",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section E",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section F",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+    ],
+    stats: {},
+  },
+
+  {
+    name: "Programming in C Lab",
+    code: "CSC 184",
+    classes: [
+      {
+        name: "Section A",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -38,7 +144,52 @@ export const courses = [
       },
 
       {
-        name: "Section B",
+        name: "Section C",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section D",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section E",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section F",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -54,13 +205,12 @@ export const courses = [
     ],
     stats: {},
   },
-
   {
-    name: "Programming in C",
-    code: "CSC 131",
+    name: "Programming in C++",
+    code: "CSC 283",
     classes: [
       {
-        name: "Section B",
+        name: "Section A",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -71,7 +221,11 @@ export const courses = [
             context: "Student",
           },
         ],
-        stats: {},
+        stats: {
+          "Can't comprehend what the faculty's saying!": "60.00%",
+          "Don't understand the topics in English!": "20.00%",
+          "Lessons are fast-paced!": "20.00%",
+        },
       },
 
       {
@@ -90,7 +244,52 @@ export const courses = [
       },
 
       {
-        name: "Section B",
+        name: "Section C",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section D",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section E",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section F",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -106,13 +305,12 @@ export const courses = [
     ],
     stats: {},
   },
-
   {
-    name: "Programming in C",
-    code: "CSC 131",
+    name: "Programming in C++ Lab",
+    code: "CSC 284",
     classes: [
       {
-        name: "Section B",
+        name: "Section A",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -123,7 +321,11 @@ export const courses = [
             context: "Student",
           },
         ],
-        stats: {},
+        stats: {
+          "Can't comprehend what the faculty's saying!": "60.00%",
+          "Don't understand the topics in English!": "20.00%",
+          "Lessons are fast-paced!": "20.00%",
+        },
       },
 
       {
@@ -142,7 +344,52 @@ export const courses = [
       },
 
       {
-        name: "Section B",
+        name: "Section C",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section D",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section E",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section F",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -158,13 +405,12 @@ export const courses = [
     ],
     stats: {},
   },
-
   {
-    name: "Programming in C",
-    code: "CSC 131",
+    name: "Fundamentals of Electronics & Digital Systems",
+    code: "CSC 231",
     classes: [
       {
-        name: "Section B",
+        name: "Section A",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -175,7 +421,11 @@ export const courses = [
             context: "Student",
           },
         ],
-        stats: {},
+        stats: {
+          "Can't comprehend what the faculty's saying!": "60.00%",
+          "Don't understand the topics in English!": "20.00%",
+          "Lessons are fast-paced!": "20.00%",
+        },
       },
 
       {
@@ -194,7 +444,52 @@ export const courses = [
       },
 
       {
-        name: "Section B",
+        name: "Section C",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section D",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section E",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section F",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -210,13 +505,12 @@ export const courses = [
     ],
     stats: {},
   },
-
   {
-    name: "Programming in C",
-    code: "CSC 131",
+    name: "Fundamentals of Electronics & Digital Systems Lab",
+    code: "CSC 232",
     classes: [
       {
-        name: "Section B",
+        name: "Section A",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
@@ -227,7 +521,11 @@ export const courses = [
             context: "Student",
           },
         ],
-        stats: {},
+        stats: {
+          "Can't comprehend what the faculty's saying!": "60.00%",
+          "Don't understand the topics in English!": "20.00%",
+          "Lessons are fast-paced!": "20.00%",
+        },
       },
 
       {
@@ -246,7 +544,152 @@ export const courses = [
       },
 
       {
+        name: "Section C",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section D",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section E",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section F",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+    ],
+    stats: {},
+  },
+  {
+    name: "Data Structure & Algorithms",
+    code: "CSC 391",
+    classes: [
+      {
+        name: "Section A",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {
+          "Can't comprehend what the faculty's saying!": "60.00%",
+          "Don't understand the topics in English!": "20.00%",
+          "Lessons are fast-paced!": "20.00%",
+        },
+      },
+
+      {
         name: "Section B",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section C",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section D",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section E",
+        faculty: {
+          name: "Faculty Name",
+          context: "Faculty",
+        },
+        students: [
+          {
+            name: "Student Name",
+            context: "Student",
+          },
+        ],
+        stats: {},
+      },
+
+      {
+        name: "Section F",
         faculty: {
           name: "Faculty Name",
           context: "Faculty",
