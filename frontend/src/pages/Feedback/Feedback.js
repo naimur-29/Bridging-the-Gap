f23e7c21720c0f685c1d01cb1e870e957f1d013f
Naim <skylike2904@gmail.com>
2023-03-15 00:35:41 +0600

    up

--- frontend/src/pages/Feedback/Feedback.js
+++ frontend/src/pages/Feedback/Feedback.js
@@ -10,11 +10,26 @@ const Feedback = ({ setNavbarTitle }) => {
 
   return (
     <div className="main-container">
-      <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100   mt-[160px]">
+      <div className="card flex-shrink-0 w-[90vw] max-w-lg shadow-2xl bg-base-100 mt-[100px] md:mt-[160px] md:w-full">
         <div className="card-body">
           <div className="form-control">
+            <select className="select select-primary w-full">
+              <option disabled selected>
+                Find your problems / suggestions
+              </option>
+              <option>Facing problems to understand accent</option>
+              <option>Prefer to write down more on the board</option>
+              <option>Solve the practice problems more often</option>
+              <option>
+                Couldn't find the course materials in University Library
+              </option>
+              <option>Faculty may vary their teaching methods </option>
+            </select>
+          </div>
+
+          <div className="form-control">
             <label className="label">
-              <span className="label-text">Write down your problems</span>
+              <span className="label-text">Or write down your own</span>
             </label>
             <textarea
               rows={4}
--- frontend/src/pages/Home/components/LandingInfo/LandingInfo.js
+++ frontend/src/pages/Home/components/LandingInfo/LandingInfo.js
@@ -7,7 +7,7 @@ const LandingInfo = ({ setIsStudent }) => {
       <div className="hero-content text-center text-neutral-content">
         <div className=" transition-all duration-200 max-w-md lg:max-w-screen-2xl">
           <h1 className="mb-5 text-5xl font-bold lg:text-6xl">
-            Welcome to Bridging the Gap
+            Bridging the Gap
           </h1>
           <p className="mb-5 text-xl lg:mb-10 mt-[-10px]">
             Sign up today and start building stronger connections with your
@@ -16,14 +16,14 @@ const LandingInfo = ({ setIsStudent }) => {
 
           <div className="btn-container flex-col mx-5 lg:flex-row">
             <button
-              className="btn btn-primary w-full mb-3 text-xl lg:w-[30%] mr-3"
+              className="btn btn-primary w-full mb-3 text-xl lg:w-[35%] mr-3"
               onClick={() => setIsStudent("student/faculty")}
             >
               Student/Faculty
             </button>
 
             <button
-              className="btn btn-primary w-full text-xl lg:w-[30%]"
+              className="btn btn-primary w-full text-xl lg:w-[35%]"
               onClick={() => {
                 setIsStudent("university");
               }}
